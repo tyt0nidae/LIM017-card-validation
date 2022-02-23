161c1611944f957ad75dde94afb4641875bb461c
tyt0nidae <mcnavarro957@gmail.com>
2022-02-23 11:43:25 -0500

    reemplazar letra x numeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const formulario = document.querySelector('#formulario-tarjeta');
+const numeroTarjeta = document.querySelector('#tarjeta .numero');
+
+formulario.inputNumero.addEventListener('keyup', (e) => {
+	let valorInput = e.target.value;
+	formulario.inputNumero.value = valorInput
+	.replace(/\s/g, '')
+    .replace(/\D/g, '')
+    .replace(/([0-9]{4})/g, '$1 ')
+    .trim();
+  numeroTarjeta.textContent = valorInput;
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,10 @@
 const validator = {
-  // ...
+	isValid: function(cc) {	
+		return cc;
+	}, 
+	maskify: function(cc) {
+		return cc;
+	}
 };
 
 export default validator;
